10bdb546e2393d7d3fa34582b39730e364a1791d
Lezwon <lezwon@gmail.com>
2017-10-29 09:53:17 +0000

    Added make docker

--- Makefile
+++ Makefile
@@ -6,3 +6,11 @@ test:
 import-abi:
 	npm install && \
 	cp node_modules/@openmined/sonar/build/*.abi abis/
+    
+docker:
+	if [ "$(sudo docker images -q capsule_notebook 2> /dev/null)" == "" ]; then \
+		if [ ! -d ~/Capsule ]; then git clone "https://github.com/lezwon/Capsule.git" ~/Capsule; fi; \
+		echo "hello" \
+		sudo docker build ~/Capsule -t capsule_notebook; \
+	fi
+	sudo docker-compose up
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-/bin/ethermint --datadir /.ethermint/setup init /.ethermint/setup/genesis.json
-/bin/ethermint --datadir /.ethermint/setup --rpc --rpcaddr=0.0.0.0 --ws --wsaddr=0.0.0.0 --rpcapi eth,net,web3,personal,admin
+
+case "$1" in
+    "ethermint")
+        rm -rf /.ethermint/setup/ethermint
+        /bin/ethermint --datadir /.ethermint/setup init /.ethermint/setup/genesis.json
+        /bin/ethermint --datadir /.ethermint/setup --rpc --rpcaddr=0.0.0.0 --ws --wsaddr=0.0.0.0 --rpcapi eth,net,web3,personal,admin
+    ;;
+        
+    "tendermint")
+        rm -rf /tendermint/*
+        /bin/tendermint init
+        /bin/tendermint node
+    ;;
+esac
+    
